f2f8050fa346689d221c72dd7d2aff28ef067a8f
QuiteAir <hfvbkmr@mail.ru>
2021-03-01 15:00:59 +0300

    feat: implement function that reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
   
+  
+  let result = '',
+  str = (n >= 0) ? 
+    n.toString() : 
+    (n * -1).toString();
+ 
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return +result;
 }
